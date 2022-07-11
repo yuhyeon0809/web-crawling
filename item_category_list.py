b09a11ad10b9a8c334fb95fc2118c0852d47cc6f
yuhyeon0809 <uvaaaan0809@naver.com>
2022-07-11 17:30:20 +0900

    카테고리별 탐색 기능 추가

---
+++ item_category_list.py
@@ -0,0 +1,77 @@
+import requests 
+import pandas as pd
+from bs4 import BeautifulSoup
+import openpyxl as op
+import csv
+
+def read_list(list_xlsx):
+    df = pd.read_excel(list_xlsx, usecols = "A")
+    item_list = df.values.tolist()
+    return item_list
+
+def write_csv(item_list, writer):
+
+    for group in item_list:
+        search = str(group)
+        search = search[2:-2]
+        print(search)
+
+        url = "https://www.daisomall.co.kr/shop/search.php?nset=1&max=50&search_text={}&orderby=daiso_ranking1".format(search)
+        
+        res = requests.get(url)
+        res.raise_for_status()
+        
+        soup = BeautifulSoup(res.text, "lxml")
+        try:
+            max_item = int(soup.find("span", {"class": "font_normal size_16"}).text)
+        except:
+            max_item = 0
+
+        categories = []
+        for categoryBox in soup.find_all("li", {"class":"float01"}):
+            try:
+                category = categoryBox.find('a').text
+                print(category)
+                categories.append(category)
+            except:
+                break
+
+        data = [search, "총 상품 " + str(max_item) + "개"]
+        for i in range(0, len(categories)):
+            data.append(categories[i])
+
+        writer.writerow(data)
+
+def csvtoxlsx(filename_csv):
+    wb = op.Workbook()
+    ws = wb.active
+    with open(filename_csv, 'r', encoding='utf8') as f:
+        for row in csv.reader(f):
+            ws.append(row)
+    wb.save('item_category_list.xlsx')
+
+    wb = op.load_workbook('item_category_list.xlsx') 
+    ws = wb.active
+    ws.column_dimensions['A'].width = 15
+    ws.column_dimensions['B'].width = 15
+    
+    for i in range(0, 10):
+        ws.column_dimensions[chr(67+i)].width = 20
+
+    wb.save('item_category_list.xlsx')
+    
+
+if __name__ == "__main__":
+
+    filename_csv = "item_category_list.csv"  # 결과를 저장할 csv 파일 이름
+
+    f = open(filename_csv, "w", encoding="utf-8-sig", newline="")
+    writer = csv.writer(f)
+
+
+    # 컬럼 이름 지정
+    item_list = read_list("item_list.xlsx")
+    write_csv(item_list, writer)
+    f.close()
+    csvtoxlsx(filename_csv)
+        
---
+++ item_category_test.py
@@ -0,0 +1,149 @@
+import csv
+import requests 
+from bs4 import BeautifulSoup
+import openpyxl as op
+import pandas as pd
+import urllib.request
+
+toCid = {'수납/정리': '019000000000000',
+        '주방/욕실/청소': '020000000000000',
+        '가구/인테리어': '022000000000000',
+        '사무/문구/디지털': '021000000000000',
+        '가전/레져/식품': '023000000000000',
+        '키즈/뷰티/패션잡화': '024000000000000',
+        '다이소 매장상품': '010000000000000',
+        '포장재 전문관': '027000000000000'}
+
+def write_csv(item_list_xlsx, imgPath, writer):
+
+    wb = op.load_workbook(item_list_xlsx) # 엑셀파일 열기
+    ws = wb.active
+ 
+    row_max = ws.max_row # 최대행값 저장
+
+    for r in range(25, row_max+1):
+        categories = []
+        search = str(ws.cell(row=r, column=1).value)
+        print("search = "+ search)
+
+        for c in range(3, ws.max_column+1):
+            category = str(ws.cell(r, c).value)
+            print(category)
+            if category == 'None':
+                continue
+            categories.append(category)
+        print(categories)
+        for category in categories:
+            index = str(category).find('(')
+            category = category[1:index]
+            try:
+                cid = toCid[category]
+                print("category = " + category + " cid = " + cid)
+            except:
+                print(category + "---unknown category!")
+                continue
+
+            pageUrl = "https://www.daisomall.co.kr/shop/search.php?search_text={}&cid={}&depth=1&list_type=&sub_list_type=&max=50".format(search, cid)
+            pageRes = requests.get(pageUrl)
+            pageRes.raise_for_status()
+            pageSoup = BeautifulSoup(pageRes.text, "lxml")
+            try:
+                max_item = int(pageSoup.find("span", {"class": "font_normal size_16"}).text)
+                max_page = int(max_item/50) + 1
+            except:
+                continue
+
+            for page in range(1, max_page+1): 
+                print(page)
+                url = "https://www.daisomall.co.kr/shop/search.php?nset=1&page={}&max=50&search_text={}&orderby=daiso_ranking1&cid={}&depth=1".format(page, search, cid)
+                res = requests.get(url)
+                res.raise_for_status()
+                soup = BeautifulSoup(res.text, "lxml")
+
+                items = soup.find_all("li", {"class": "float01 search_goods_list"})
+
+                i = 1
+                for item in items:
+                    title = item.find('div', {"style": "margin-top:10px;height:38px;"})
+
+                    itemName = title.find('a').get("title")
+                    if itemName.find("<b>") == -1:  # 상품명에 검색어가 포함되지 않은 항목 제외
+                        continue
+                    if itemName.find("밀크북") != -1:
+                        continue
+                    itemName = itemName.replace("<b>", '')
+                    itemName = itemName.replace("</b>", '')
+                    print(itemName)
+
+                    price = item.find('div', {"style": "margin-top:12px;"})
+                    itemPrice = price.find('strong').text
+                    itemPrice = itemPrice.replace("원", '')
+                    itemPrice = itemPrice.replace(",", '')
+
+                    img = item.find('div', {"class": "goods_line_img"})
+                    imgUrl = img.find('img').get('src')
+                    imgName = "{}{}.jpg".format(search, (page-1)*50+i)
+
+                    # 물품 이미지 다운로드
+                    urllib.request.urlretrieve(imgUrl, imgPath+imgName)
+
+                    itemId = item.find('a').get('href')
+                    itemId = itemId[24:34]
+                    itemUrl = "https://www.daisomall.co.kr/shop/goods_view.php?id={}&depth=1&search_text={}".format(itemId, search)
+                    
+                    itemRes = requests.get(itemUrl)
+                    itemRes.raise_for_status()
+                    soupItem = BeautifulSoup(itemRes.text, "lxml")
+
+                    itemCategories = []
+                    try:
+                        itemNum = soupItem.find('td', {"class": "color_63 line_h160"}).find('strong').text
+                    except:
+                        print("today deal error!")
+                        continue
+                    
+                    for itemCategory in soupItem.find_all('option', {"selected": ""}):
+                        if str(itemCategory).find("selected") == -1:
+                            continue
+                        itemCategory = itemCategory.get_text()
+                        itemCategories.append(itemCategory)
+
+                    try:
+                        data = [None, search, (page-1)*50+i, itemNum, itemCategories[0]+">"+itemCategories[1]+">"+itemCategories[2], itemName, imgUrl, itemPrice]
+                    except:
+                        print("category error!")
+                        continue
+                    writer.writerow(data)
+                    i+=1
+
+
+# csv파일을 엑셀파일로 변환
+def csvtoxlsx(filename_csv, filename_xlsx):
+    wb = op.Workbook()
+    ws = wb.active
+    with open(filename_csv, 'r', encoding='utf8') as f:
+        for row in csv.reader(f):
+            ws.append(row)
+    wb.save(filename_xlsx)
+
+
+# main 함수
+if __name__ == "__main__":
+    
+    item_list_xlsx = "item_category_list.xlsx"
+    filename_csv = "item_category.csv"          # 결과를 저장할 csv 파일 이름
+    filename_xlsx = "item_category.xlsx"        # 결과를 저장할 xlsx 파일 이름
+    imgPath = "item_img/"                       # 이미지 파일이 저장될 경로
+    
+
+    # f = open(filename_csv, "a", encoding="utf-8-sig", newline="")
+    # writer = csv.writer(f)
+
+    # # 컬럼 이름 지정
+    # columns_name = ["물품분류", "물품종", "순번", "상품번호", "카테고리", "상품명", "상품사진", "가격"] 
+    # writer.writerow(columns_name)
+
+    # write_csv(item_list_xlsx, imgPath, writer)
+    # f.close()
+    csvtoxlsx(filename_csv, filename_xlsx)
+# 5027

5af8b9f4b14e79564b21b3cb0fbbd70f06265a3a
yuhyeon0809 <uvaaaan0809@naver.com>
2022-07-19 17:27:20 +0900

    세부 수정

--- item_category.py
+++ item_category.py
@@ -4,7 +4,7 @@ import openpyxl as op
 import urllib.request
 import pandas as pd
 
-# cid(카테고리 id 정의)
+# cid(카테고리 id) 정의
 toCid = {'수납/정리': '019000000000000',
         '주방/욕실/청소': '020000000000000',
         '가구/인테리어': '022000000000000',
@@ -16,20 +16,18 @@ toCid = {'수납/정리': '019000000000000',
 
 def write_data(item_list_xlsx, filename_xlsx, imgPath):
 
-    input_wb = op.load_workbook(item_list_xlsx)  # 입역을 읽어올 엑셀파일
-    input_ws = input_wb['Sheet1']
+    input_wb = op.load_workbook(item_list_xlsx)  # 입력을 읽어올 엑셀파일
+    input_ws = input_wb['위해물품']
     row_max = input_ws.max_row # 최대행값 저장
 
     output_wb = op.load_workbook(filename_xlsx)  # 결과를 저장할 엑셀파일
     output_ws = output_wb.active
 
-    type_cnt = 0
     for r in range(2, row_max+1):  # 2행부터 마지막행까지 반복
         categories = []
         type = str(input_ws.cell(row=r, column=1).value)  # 물품분류
-        if type != 'None':
-            type_cnt+=1
-
+        if type == 'None':
+            type = str(input_ws.cell(row=r-1, column=1).value)
         search = str(input_ws.cell(row=r, column=2).value)  # 검색어
         if search == 'None':
             continue
@@ -43,14 +41,14 @@ def write_data(item_list_xlsx, filename_xlsx, imgPath):
                 continue
             categories.append(category)
 
-        print("----------" + search + "----------")
+        print("-------------" + search + "-------------")
         num = 1  # 순번
         for category in categories:  # 해당 검색어의 카테고리 하나씩 탐색
             index1 = category.find('(')
             index2 = category.find(')')
             itemNum = int(category[index1+1:index2].replace(',', ''))
             categoryPage = int(itemNum/50) + 1
-            if categoryPage > 150:
+            if categoryPage > 150:  # 150 페이지가 넘어가는 경우 150 페이지까지만 탐색
                 categoryPage = 150
             category = category[:index1]
             try:
@@ -59,7 +57,7 @@ def write_data(item_list_xlsx, filename_xlsx, imgPath):
                 print(category + "---unknown category!")
                 continue
 
-            print("----------" + category + "----------")
+            print("-------------" + category + "-------------")
             for page in range(1, categoryPage+1):  # 1페이지부터 마지막 페이지까지 반복
                 print(page)
 
@@ -69,10 +67,10 @@ def write_data(item_list_xlsx, filename_xlsx, imgPath):
                 res.raise_for_status()
                 soup = BeautifulSoup(res.text, "lxml")
 
-                # 해당 페이지에 나와 있는 상품 목록을 저장
+                # 해당 페이지에 나와 있는 상품을 모두 저장
                 items = soup.find_all("li", {"class": "float01 search_goods_list"})
 
-                # 상품 목록에 있는 상품을 하나씩 탐색
+                # 상품을 하나씩 탐색
                 for item in items:
                     title = item.find('div', {"style": "margin-top:10px;height:38px;"})
                     itemName = title.find('a').get("title")  # 상품명
@@ -123,6 +121,7 @@ def write_data(item_list_xlsx, filename_xlsx, imgPath):
                     try:
                         itemRes = requests.get(itemUrl, headers={'User-Agent':'Mozilla/5.0'})
                     except:
+                        print("request error!")
                         continue
                     itemRes.raise_for_status()
                     soupItem = BeautifulSoup(itemRes.text, "lxml")
@@ -134,14 +133,12 @@ def write_data(item_list_xlsx, filename_xlsx, imgPath):
                         continue
                     
                     itemCategories = []
-                    for itemCategory in soupItem.find_all('option', {"selected": ""}):
+                    for itemCategory in soupItem.find_all('option', {"selected": ""}):  # 해당 상품의 카테고리 저장
                         if str(itemCategory).find("selected") == -1:
                             continue
                         itemCategory = itemCategory.get_text()
                         itemCategories.append(itemCategory)
 
-                    if type == "None":
-                        type = ''
                     try:
                         data = [type, None, search, num, itemNum, itemCategories[0]+">"+itemCategories[1]+">"+itemCategories[2], itemName, imgUrl, itemPrice, itemUrl]
                     except:
@@ -150,10 +147,7 @@ def write_data(item_list_xlsx, filename_xlsx, imgPath):
                     output_ws.append(data)
                     num+=1
                     
-                output_wb.save(filename_xlsx)
-        if type_cnt >= 50:
-            output_ws = output_wb.create_sheet()
-            type_cnt = 0
+            output_wb.save(filename_xlsx)
 
 
 # 물품코드 로딩
@@ -161,7 +155,7 @@ def load_code(item_list_xlsx, filename_xlsx):
     wb = op.load_workbook(filename_xlsx)
     ws = wb.active
     code_wb = op.load_workbook(item_list_xlsx)
-    code_ws = code_wb['Sheet2']
+    code_ws = code_wb['물품코드표']
     row_max = ws.max_row
     code_row_max = code_ws.max_row
     
@@ -177,8 +171,8 @@ def load_code(item_list_xlsx, filename_xlsx):
 
     for r in range(2, row_max+1):
         temp = str(ws.cell(row=r, column=1).value)
-        # ws.cell(row=r, column=2).value = codeDic[temp]
-        # wb.save(filename_xlsx)
+        ws.cell(row=r, column=2).value = codeDic[temp]
+        wb.save(filename_xlsx)
         print(codeDic[temp])
         
 
@@ -191,14 +185,15 @@ def drop_duplicates(filename_xlsx):
 # main 함수
 if __name__ == "__main__":
     
-    item_list_xlsx = "촬영 대상 물품_v0.8 다이소몰 크롤링 목록_카테고리.xlsx"  # 읽어올 물품 리스트
-    filename_xlsx = "촬영 대상 물품_v0.8 다이소몰 크롤링 결과 리스트2.xlsx"  # 결과를 저장할 xlsx 파일 이름
+    item_list_xlsx = "촬영 대상 물품 분류체계_v0.1_권혁진_다이소몰 크롤링 목록_위해물품_정보저장매체.xlsx"  # 읽어올 물품 리스트
+    filename_xlsx = "촬영 대상 물품 분류체계_v0.1_권혁진_다이소몰 크롤링 결과_위해물품_정보저장매체.xlsx"  # 결과를 저장할 xlsx 파일 이름
     imgPath = "item_img/"  # 이미지 파일이 저장될 경로
     columns_name = ["물품분류", "물품코드", "물품종", "순번", "상품번호", "카테고리", "상품명", "상품사진", "가격", "링크"]  # 컬럼명 지정
+    sheet_name = '위해물품'
 
     # #--- 새 엑셀파일 생성 시
     output_wb = op.Workbook()
-    output_ws = output_wb.active
+    output_ws = output_wb.create_sheet(sheet_name)
     output_ws.append(columns_name)
     output_wb.save(filename_xlsx)
 
@@ -210,6 +205,6 @@ if __name__ == "__main__":
     # output_ws = output_wb.create_sheet()
     # output_wb.save(filename_xlsx)
 
-    write_data(item_list_xlsx, filename_xlsx, imgPath)
+    write_data(item_list_xlsx, filename_xlsx, imgPath, sheet_name)
     # drop_duplicates(filename_xlsx)
     # load_code(item_list_xlsx, filename_xlsx)
